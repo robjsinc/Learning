3f30598efe25117b334902db12134cd74a19c7de
Rob Sinclair <robjsinc@gmail.com>
2020-04-20 18:57:53 +0100

    firstcommit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LearningConApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
